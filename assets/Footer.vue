569ae8f4cf8a3e3510e8ed5db47895eb5e63c5d9
2badao <34239476+2badao@users.noreply.github.com>
2026-09-21 16:34:38 +0800

    Remove template and script from Footer.vue
    Removed template and script sections from Footer.vue.

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -1,21 +1,3 @@
-<template>
-  
-</template>
-
-<script>
-export default {
-  name: "Footer",
-  data() {
-    return {
-      homeUrl: "https://www.liushen.fun/",
-      blogUrl: "https://blog.liushen.fun/",
-      githubUrl: "https://github.com/willow-god",
-      emailUrl: "mailto:01@liushen.fun"
-    };
-  }
-};
-</script>
-
 
 <style scoped>
 .footer {
